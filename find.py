2e5b5e8dc4b76b35bacd9a46d8ea8f81c731d5cc
jschump <98626959+jschump@users.noreply.github.com>
2022-05-16 19:56:12 -0700

    Add files via upload

---
+++ find.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+import subprocess
+
+ls = "ls"
+
+#lscheck = subprocess.Popen([ls, '/opt/appdynamics/machine-agent/'], capture_output=True)
+
+
+lscheck = subprocess.run([ls, '/opt/appdynamics/machine-agent/'])
+
+print(lscheck)
+if lscheck.returncode == 0:
+    print('yes')
+else:
+    print('bad')
+
+#find = "find"
+
+#finder = subprocess.Popen([find, '
+
+
+
+#1) come up with a way to find if the machine is running appdynamis, check services then location. If neither show up end script
+
+#2) take the output of the log4js file and parse it so it you can the csv
+
+#3) check the log4js to the csv file
+
+#4) if there is a error found you have to notify the user running the script and to append to a csv the name of the server and the log4j error they have/what version they need to upgrade to
+~                                                                                                                                                                                                  
